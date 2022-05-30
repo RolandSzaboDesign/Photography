37397d1c5bb606d2f55a3f2993253fada9dc6a86
Roland Szabó <hi@rolandszabo.design>
2022-05-30 09:47:56 +0200

    v5.2
    Lightbox WIP

---
+++ public/assets/js/lightbox.js
@@ -0,0 +1,20 @@
+function open() {
+	document.getElementByClassName("lightbox").style.display = "block";
+}
+
+function close() {
+	document.getElementByClassName("lightbox").style.display = "none";
+}
+
+function navigate(n) {
+	start(index += n);
+}
+
+function start(n) {
+	var i;
+	var slides = document.getElementByClassName
+}
+
+let index = 1;
+
+start(index);
