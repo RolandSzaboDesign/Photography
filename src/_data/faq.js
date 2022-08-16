ecc205cd7dbc054ed81a6eb501106f2c997bd950
Roland Szabó <hi@rolandszabo.design>
2022-08-16 16:47:52 +0200

    WIP

---
+++ src/_data/faq.js
@@ -0,0 +1,34 @@
+module.exports = {
+	items: [
+		{
+			hu: {
+				question: "Nem vagyok valami fotogén, segítesz majd pózolni a fotózáskor?",
+				answer: "A legtöbben nem vagyunk szakképzett modellek, akik tudják, hogy hogyan nézzenek ki a legjobban a fotókon. A legtöbb ember a galériámban pontosan ugyanígy érezte magát a fotózás előtt. Természetesen, segítek a pózokban, de ez nem jelenti azt, hogy a képeid pózoltnak fognak kinézni a végén. Inkább érzéseket igyekszem megörökíteni, amihez laza, időnként vicces dolgokat kérek Tőled. Ezekből az érzésekből lesznek a legtermészetesebb fotók, amiket a legjobban fogsz szeretni!",
+			},
+			en: {
+				question: "I'm not very photogenic, can you help me with posing?",
+				answer: "Most of us aren't models who knwo how they'll look their best on photos. That's exactly how most people in my galleries felt before the photoshoot. Certainly, I'll help you with posing, but that doesn't mean your photos will look posed in the end. I much prefet to capture pure emotions, for which I'll give you some laid-back, fun, and sometimes silly prompts. These will be the photos with the purest emotions which you'll love the most!",
+			},
+		},
+		{
+			hu: {
+				question: "Mennyi idő múlva kapom meg az elkészült képeket?",
+				answer: "Általában 1-3 munkanap alatt el szoktam készülni a fotóiddal, onnantól számítva, hogy elküldted nekem az általad választott képeket. Attól függően, hogy éppen mennyire vagyok tele fotózásokkal és szerkesztésekkel, ez az időtartam változhat, amiről időben tájékoztatni foglak.",
+			},
+			en: {
+				question: "How long do I need to wait for my edited photos?",
+				answer: "Usually I'll send you the finished photos in 1-3 working days, after you sent me the ones you've selected for editing. Depending on how busy I am with shooting and editing at the given time, this interval might change, but I'll let you know about this in advance.",
+			},
+		},
+		{
+			hu: {
+				question: "",
+				answer: "",
+			},
+			en: {
+				question: "",
+				answer: "",
+			},
+		},
+	]
+}
