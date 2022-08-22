3d797ca64896822bb2cb082fe525c07f8ba6a0e1
Roland Szabó <hi@rolandszabo.design>
2022-08-22 14:03:30 +0200

    v5.2.24

--- src/_data/faq.js
+++ src/_data/faq.js
@@ -7,7 +7,19 @@ module.exports = {
 			},
 			en: {
 				question: "I'm not very photogenic, can you help me with posing?",
-				answer: "Most of us aren't models who knwo how they'll look their best on photos. That's exactly how most people in my galleries felt before the photoshoot. Certainly, I'll help you with posing, but that doesn't mean your photos will look posed in the end. I much prefet to capture pure emotions, for which I'll give you some laid-back, fun, and sometimes silly prompts. These will be the photos with the purest emotions which you'll love the most!",
+				answer: "Most of us aren't models who knwo how they'll look their best on photos. That's exactly how most people in my galleries felt before the photoshoot. Certainly, I'll help you with posing, but that doesn't mean your photos will look posed in the end. I much prefer to capture pure emotions, for which I'll give you some laid-back, fun, and sometimes silly prompts. These will be the photos with the purest emotions which you'll love the most!",
+			},
+		},
+		{
+			hu: {
+				question: "Hogyan kapom meg a végleges képeket?",
+				answer: "Az elkészült képeket digitálisan, egy Google Drive vagy Dropbox mappában, publikus linkként fogod megkapni. Ez azt jelenti, hogy csak a link birtokában lévő emberek látják a mappa tartalmát (vagyis Te és én), így nem szükséges az email címedet megadnod a fotóid eléréséhez. Innen le tudod tölteni az összes képet eredeti minőségben, amiket utána ki is tudsz nyomtatni tetszésed szerinti méretben.",
+			},
+			en: {
+				question: "How will I get the finished photos after the shoot?",
+				answer: "",
+				// DELETE WHEN DONE
+				original: "Your photos will be delivered to you in an online gallery on my website, from the gallery you can print, view, and download high resolution files to your heart’s content. You have the option to password restrict your gallery or keep it openly viewable to make it easier for friends and family to view.",
 			},
 		},
 		{
@@ -22,11 +34,213 @@ module.exports = {
 		},
 		{
 			hu: {
-				question: "",
+				question: "Hol sétálunk?",
+				answer: "Arra megyünk, amerre szeretnél, Budapesten belül. Én a fotós helyszínek miatt a belvárost részesítem előnyben, de nagyon szívesen elmegyek egy erdőbe vagy természetvédelmi területre, ha természetközeli képeket szeretnél.",
+			},
+			en: {
+				question: "Where do we walk?",
+				answer: "We can walk and shoot wherever you want in Budapest. I prefer the inner districts for photographic possibilities, but I don't shy away from going to a nearby nature reserve or forest to take more close-to-nature photos. I'm available to shoot the entire day as well, however, in this case, a special pricing will be agreed upon.",
+			},
+		},
+		{
+			hu: {
+				question: "Milyen hosszú egy fotós séta?",
+				answer: "Általában 30 és 120 perc között szokott lenni. Elég rugalmas tudok lenni az idővel, úgyhogy eldöntheted, hogy csak egy pár képre van szükséged, vagy szeretnél többet kint lenni, meglátogatni több helyszínt, akár átöltözni is közben. Benne vagyok egésznapos fotózásban is, aminek a díjazását közösen megbeszéljük.",
+			},
+			en: {
+				question: "How long is a photo walk?",
+				answer: "It usually ranges from 30 minutes to 120 minutes or even more. I'm pretty versatile on time, so you can decide if you just want a few headshots taken or stay longer and visit multiple locations and even change outfits.",
+			},
+		},
+		{
+			hu: {
+				question: "Hogy működik a képszerkesztés?",
+				answer: "Minden általad kiválasztott fotót színkezelni fogok oly módon, hogy az tükrözze a személyiségedet, a helyszínt, és a fotózásunk témáját. A retusálásba beletartozik a bőr tökéletlenségek eltávolítása, illetve az esetleges babahajak eltávolítása a képekről.",
+			},
+			en: {
+				question: "Do you edit the photos?",
+				answer: "All photos you select after the shoot will be color-graded in a way that matches your personality, the location, and the theme of the shoot. Retouching includes removing skin imperfections and unnecessary flying hairs.",
+			},
+		},
+		{
+			hu: {
+				question: "Megkapom a nyers képeket is?",
+				answer: "Nem adok ki nyers képeket, csak olyanokat, amelyek tükrözik a szerkesztési stílusomat. Ez azért van, hogy az emberek ne kapjanak pontatlan képet a munkám minőségéről.",
+			},
+			en: {
+				question: "Will I get the raw photos, too?",
+				answer: "I don't hand out unedited photos, only processed ones that reflect my personal editing style. This is to prevent people getting an inaccurate representation of the quality of my work.",
+			},
+		},
+		{
+			hu: {
+				question: "Miért fekete-fehérek és vízjelezettek a válogatásra kiadott képek?",
+				answer: "Azért fekete-fehérek, hogy jobban tudj koncentrálni a képen az arckifejezésedre, a pózra, a környezetre; nem akarom, hogy a nyers színek, amelyeken később úgyis alakítani fogok, elvegyék a kedved egy fotótól. A válogatásra kiadott képek továbbá vízjellel vannak ellátva, hogy jelöljék, hogy ezek nem véglegesen elkészített képek.",
+			},
+			en: {
+				question: "Why are the raw photos black-and-white and watermarked?",
+				answer: "The reason for <abbr title='blank-and-white'>B/W</abbr> is so that you can better concentrate on the composition, your facial expressions and poses rather than the colors, which will be processed later anyways. Your photos for selection will be watermarked to denote that they're not final images.",
+			},
+		},
+		{
+			hu: {
+				question: "Fotózhatunk stúdióban is?",
+				answer: "Természetesen. Keress meg bátran az ötleteddel, és segítek megtalálni a megfelelő stúdiót számodra. Az árazás ugyanúgy működik, mint a fotós sétáknál, viszont a stúdió bérlését Neked kell intézned.",
+			},
+			en: {
+				question: "Do you shoot in a studio as well?",
+				answer: "Absolutely. Please reach out to me with your specific idea, and we'll figure out the best studio for your photoshoot. Pricing is the same for studio portraits, but please note that paying for the studio rental is your responsibility.",
+			},
+		},
+		{
+			hu: {
+				question: "Csinálsz igazolvány/önéletrajz fotókat is?",
+				answer: "Persze! Szívesen segítek profi arcképet készíteni Rólad a LinkedIn-re, vagy a céges weboldaladra, vagy közösségi médiára. Annyi változás van itt, hogy az árazás picit másképp történik. Keress bátran az igényeiddel, szívesen segítek a tökéletes arcképed elkészítésében!",
+			},
+			en: {
+				question: "Do you do headshots?",
+				answer: "Certainly. I can help you get professional headshots for LinkedIn, or your company's website or social media. Please note that for headshots, pricing is a little different. Don't hesitate to reach out to me for details, I'll be glad to assist you!",
+			},
+		},
+		{
+			hu: {
+				question: "Adsz néhány példát az árazásra?",
+				answer: "<ul class='list'><li><strong>1. eset:</strong> Csak 5 fotóra van szükséged &mdash; 7.500 Ft.</li><li><strong>2. eset:</strong> 10 fotóra van szükséged &mdash; 15.000 Ft.</li><li><strong>3. eset:</strong> 16 fotóra van szükséged &mdash; 21.000 Ft.</li><li><strong>4. eset:</strong> 8 fotóra van szükséged és béreltünk egy autót 2.500 Ft-ért &mdash; 12.000 + 1.250 = 13.250 Ft.</li></ul>",
+			},
+			en: {
+				question: "Can I get some examples on pricing?",
+				answer: "<ul class='list'><li>You only need 5 photos &mdash; that'll be 7.500 HUF.</li><li>You need 10 photos &mdash; that's 15.000 HUF.</li><li>You need 16 photos &mdash; that's only 21.000 HUF.</li><li>You need 8 photos and we rented a car and used it for a total cost of 2500 HUF &mdash; that's 12.000 + 1.250 = 13.250 HUF.</li></ul>",
+			},
+		},
+		{
+			hu: {
+				question: "Hogyhogy nem kell előre fizetni semmit?",
 				answer: "",
 			},
 			en: {
-				question: "",
+				question: "How come I don't need to pay an upfront fee?",
+				answer: "",
+			},
+		},
+		{
+			hu: {
+				question: "Milyen módon fizethetek az elkészült képekért?",
+				answer: "Fizethetsz banki átutalással (preferált), Reolut-on, PayPal-on, vagy készpénzben. A fizetést kérlek azután kövesd el, hoyg elküldted nekem a kiválasztott képeket, és leokéztam őket.",
+			},
+			en: {
+				question: "What payment methods do you accept?",
+				answer: "I accept bank transfer (preferred), Revolut, PayPal, or cash.",
+			},
+		},
+		{
+			hu: {
+				question: "Hogyan tudok időpontot foglalni?",
+				answer: "",
+			},
+			en: {
+				question: "How can I book my shooting date?",
+				answer: "",
+			},
+		},
+		{
+			hu: {
+				question: "Vállalsz eseményfotózást is?",
+				answer: "",
+			},
+			en: {
+				question: "Do you also shoot events?",
+				answer: "",
+			},
+		},
+		{
+			hu: {
+				question: "Este szeretnék fotózni, megoldható?",
+				answer: "",
+			},
+			en: {
+				question: "I'd like to shoot at night, is it possible?",
+				answer: "",
+			},
+		},
+		{
+			hu: {
+				question: "Tudunk fotózni szakadó esőben/hóban/ködben is?",
+				answer: "",
+			},
+			en: {
+				question: "Can we shoot in bad weather as well?",
+				answer: "",
+			},
+		},
+		{
+			hu: {
+				question: "Az én képeim is megjelennek a webodlaladon?",
+				answer: "",
+			},
+			en: {
+				question: "Will my photos show up on your website?",
+				answer: "",
+			},
+		},
+		{
+			hu: {
+				question: "Nem fognak elveszni az elkészített fotók?",
+				answer: "",
+			},
+			en: {
+				question: "What's your backup poilcy for your photos?",
+				answer: "",
+				// DELETE WHEN DONE
+				original: "I only shoot with cameras that have dual memory card slots, so the moment I take a photo it is instantly written to 2 memory cards. In the event of a memory card failure, I have a full backup so no images are lost. Once images are brought back to my studio, they are downloaded to my computer, immediately backed up to my on site backup hard drive, and once every week I bring in an additional backup drive from my home and update it with my most recent photos to ensure I have an offsite backup in the event of a fire or theft.",
+			},
+		},
+		{
+			hu: {
+				question: "Hány napra előre kell időpontot foglaljak?",
+				answer: "",
+			},
+			en: {
+				question: "How far in advance should I book?",
+				answer: "",
+			},
+		},
+		{
+			hu: {
+				question: "Szeretnék egész nap fotózni, különböző helyszíneken, megoldható?",
+				answer: "",
+			},
+			en: {
+				question: "Is it possible to shoot all day in multiple locations?",
+				answer: "",
+			},
+		},
+		{
+			hu: {
+				question: "Mennyibe kerül az utazás Budapesten kívül?",
+				answer: "",
+			},
+			en: {
+				question: "What do you charge for travel outside of Budapest?",
+				answer: "",
+			},
+		},
+		{
+			hu: {
+				question: "Megoszthatom a kapott képeket az interneten?",
+				answer: "",
+			},
+			en: {
+				question: "Can I share the final photos online?",
+				answer: "",
+			},
+		},
+		{
+			hu: {
+				question: "Kinyomtathatom a képeket magamnak?",
+				answer: "",
+			},
+			en: {
+				question: "Can I print the final photos?",
 				answer: "",
 			},
 		},
