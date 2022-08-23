77a9371596b932d86692ad36966e3f165f2adb26
Roland Szabó <hi@rolandszabo.design>
2022-08-23 13:39:33 +0200

    v5.2.28.1
    Tag hotfix

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,16 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addWatchTarget("src/assets/css");
 	eleventyConfig.setQuietMode(true);
 
+	eleventyConfig.addCollection('tagList', collections => {
+		const tags = collections
+			.getAll()
+			.reduce((tags, item) => tags.concat(item.data.tags), [])
+			.filter(tag => !!tag)
+			.filter(tag => tag !== 'post')
+			.sort();
+		return Array.from(new Set(tags))
+	});
+
 	return {
 		dir: {
 			input: "src",
