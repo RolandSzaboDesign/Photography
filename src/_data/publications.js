7ba33872b31843b1e31ae4e751177514ac1aa892
Roland Szabó <hi@rolandszabo.design>
2023-12-04 17:28:04 +0100

    v6.2.24
    Added Publications to About Page

---
+++ src/_data/publications.js
@@ -0,0 +1,76 @@
+module.exports = {
+	list: [
+		// {
+		// 	title: "",
+		// 	image: "",
+		// 	en: {
+		// 		"description": "",
+		// 		"issue": "",
+		// 	},
+		// 	hu: {
+		// 		"description": "",
+		// 		"issue": "",
+		// 	}
+		// },
+		{
+			title: "Moveux",
+			image: "/assets/images/publications/moveux-23-nov.webp",
+			en: {
+				"description": "Publication",
+				"issue": "November 2023",
+			},
+			hu: {
+				"description": "Megjelenés",
+				"issue": "2023 November",
+			}
+		},
+		{
+			title: "UnTold",
+			image: "/assets/images/publications/untold-23-nov.webp",
+			en: {
+				"description": "Front Cover",
+				"issue": "November 2023",
+			},
+			hu: {
+				"description": "Címlap",
+				"issue": "2023 November",
+			}
+		},
+		{
+			title: "Proficient",
+			image: "/assets/images/publications/proficient-23-nov.webp",
+			en: {
+				"description": "Front & Back Cover",
+				"issue": "November 2023, issue 141",
+			},
+			hu: {
+				"description": "Címlap és Hátlap",
+				"issue": "2023 November, 141-ik kiadás",
+			}
+		},
+		{
+			title: "Fashion Mannuscript",
+			image: "/assets/images/publications/fashion-mannuscript-23-sep.webp",
+			en: {
+				"description": "Publication",
+				"issue": "September 2023",
+			},
+			hu: {
+				"description": "Megjelenés",
+				"issue": "2023 Szeptember",
+			}
+		},
+		{
+			title: "Dreamy",
+			image: "/assets/images/publications/dreamy-23-sep.webp",
+			en: {
+				"description": "Front Cover",
+				"issue": "September 2023, issue 584",
+			},
+			hu: {
+				"description": "Címlap",
+				"issue": "2023 Szeptember, 584-ik kiadás",
+			}
+		},
+	]
+}
